b7f8418419b8b5afda268f553d3efff94e6b0d5b
Austin Wise <AustinWise@gmail.com>
2016-06-08 00:34:26 -0700

    Simpler update script.

--- update.cmd
+++ update.cmd
@@ -1,13 +1,2 @@
 @echo off
-setlocal EnableDelayedExpansion EnableExtensions
-
-set "TOP_DIR=%~dp0"
-if %TOP_DIR:~-1%==\ set "TOP_DIR=%TOP_DIR:~0,-1%"
-
-set "ALL_REPOS=coreclr corefx core-setup cli"
-for %%i in (%ALL_REPOS%) do (
-  echo Pulling %%i
-  pushd "%TOP_DIR%\%%i"
-  git pull -nq
-  popd
-)
+git submodule foreach git pull
